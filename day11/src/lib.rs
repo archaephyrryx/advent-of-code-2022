686f4783a8f5b36e9ccc9c49490a385623a8e126
Peter Duchovni <peter.duchovni@nomadic-labs.com>
2022-12-22 14:26:03 -0500

    Day 11 finished

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -1,24 +1,28 @@
-extern crate combine;
-use combine::{Parser};
+use std::collections::HashMap;
 
 mod calc {
     #[derive(Copy, Clone, PartialEq, Eq, Hash)]
     pub enum Expr {
-        Add(u32),
-        Multiply(u32),
+        Add(u64),
+        Multiply(u64),
         Square,
     }
 
     impl std::str::FromStr for Expr {
-        type Err = <u32 as std::str::FromStr>::Err;
+        type Err = <u64 as std::str::FromStr>::Err;
 
         fn from_str(s: &str) -> Result<Self, Self::Err> {
-
+            match s.split_once(' ').unwrap() {
+                ("*", "old") => Ok(Self::Square),
+                ("*", scalar) => Ok(Self::Multiply(scalar.parse::<u64>()?)),
+                ("+", constant) => Ok(Self::Add(constant.parse::<u64>()?)),
+                _ => unreachable!(),
+            }
         }
     }
 
     impl Expr {
-        pub fn compile(self) -> Box<dyn Fn(u32) -> u32> {
+        pub fn compile(self) -> Box<dyn Fn(u64) -> u64> {
             match self {
                 Expr::Add(i) => Box::new(move |x| x + i),
                 Expr::Multiply(i) => Box::new(move |x| x * i),
@@ -30,35 +34,140 @@ mod calc {
 
 pub struct MonkeyBrain {
     id: usize,
-    behavior: Box<dyn Fn(u32) -> u32>,
-    quotient: u32,
+    behavior: Box<dyn Fn(u64) -> u64>,
+    quotient: u64,
     branch_t: usize,
     branch_f: usize,
 }
 
-impl MonkeyBrain {
-    pub fn new(id: usize, raw_expr: calc::RawExpr, quotient: u32, branch_t: usize, branch_f: usize) -> Self {
-        let behavior =
-
+impl std::fmt::Debug for MonkeyBrain {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("MonkeyBrain")
+            .field("id", &self.id)
+            .field("behavior", &"<closure>")
+            .field("quotient", &self.quotient)
+            .field("branch_t", &self.branch_t)
+            .field("branch_f", &self.branch_f)
+            .finish()
     }
-    pub fn inspect(&self, item: u32) -> (usize, u32) {
-        let inspected : u32 = (self.behavior)(item);
-        let relaxed : u32 = inspected / 3;
+}
+
+impl MonkeyBrain {
+    pub fn new(
+        id: usize,
+        expr: calc::Expr,
+        quotient: u64,
+        branch_t: usize,
+        branch_f: usize,
+    ) -> Self {
+        let behavior = expr.compile();
+        Self {
+            id,
+            behavior,
+            quotient,
+            branch_t,
+            branch_f,
+        }
+    }
+
+    pub fn get_id(&self) -> usize {
+        self.id
+    }
+
+    pub fn get_modulus(&self) -> u64 {
+        self.quotient
+    }
+
+    pub fn get_trio(&self) -> [usize; 3] {
+        [self.id, self.branch_t, self.branch_f]
+    }
+
+    pub fn inspect(&self, item: u64) -> (usize, u64) {
+        let inspected = (self.behavior)(item);
+        let relaxed = inspected / 3;
         if relaxed % self.quotient == 0 {
             (self.branch_t, relaxed)
         } else {
             (self.branch_f, relaxed)
         }
     }
+
+    pub fn inspect_mod(&self, item: u64, modulus: u64) -> (usize, u64) {
+        let inspected = (self.behavior)(item);
+        let rescaled = inspected % modulus;
+        if rescaled % self.quotient == 0 {
+            (self.branch_t, rescaled)
+        } else {
+            (self.branch_f, rescaled)
+        }
+    }
+
 }
 
-pub fn parse_monkey(lines: &mut impl Iterator<Item = std::io::Result<String>>) -> Option<(Vec<u32>, MonkeyBrain)> {
-    let mut held_items : Vec<u32> = Vec::new();
-    let _header =
-        lines
-            .next()?
-            .expect("monkey error")
-            ;
-    let mb =
-    Some()
+pub fn parse_monkey(
+    lines: &mut impl Iterator<Item = std::io::Result<String>>,
+) -> Option<(Vec<u64>, MonkeyBrain)> {
+    let mut held_items: Vec<u64> = Vec::new();
+    let id = lines
+        .next()?
+        .expect("monkey read error")
+        .strip_prefix("Monkey ")?
+        .strip_suffix(":")?
+        .parse::<usize>()
+        .expect("monkey parse error");
+    lines
+        .next()?
+        .expect("starting items error")
+        .trim_start()
+        .strip_prefix("Starting items: ")?
+        .split(", ")
+        .for_each(|item| held_items.push(item.parse::<u64>().unwrap()));
+    let expr = lines
+        .next()?
+        .expect("operation read error")
+        .trim_start()
+        .strip_prefix("Operation: new = old ")?
+        .parse::<calc::Expr>()
+        .expect("operation parse error");
+    let quotient = lines
+        .next()?
+        .expect("quotient read error")
+        .trim_start()
+        .strip_prefix("Test: divisible by ")?
+        .parse::<u64>()
+        .expect("quotient parse error");
+    let branch_t = lines
+        .next()?
+        .expect("branch_t read error")
+        .trim_start()
+        .strip_prefix("If true: throw to monkey ")?
+        .parse::<usize>()
+        .expect("branch_t parse error");
+    let branch_f = lines
+        .next()?
+        .expect("branch_f read error")
+        .trim_start()
+        .strip_prefix("If false: throw to monkey ")?
+        .parse::<usize>()
+        .expect("branch_f parse error");
+    let mb = MonkeyBrain::new(id, expr, quotient, branch_t, branch_f);
+    Some((held_items, mb))
+}
+
+pub fn parse_monkeys(
+    lines: &mut impl Iterator<Item = std::io::Result<String>>,
+) -> std::io::Result<(HashMap<usize, Vec<u64>>, Vec<MonkeyBrain>)> {
+    let mut parcels = HashMap::new();
+    let mut monkeys = Vec::new();
+
+    loop {
+        let Some((parcel, monkey)) = parse_monkey(lines) else { panic!("could not parse monkey")};
+        parcels.insert(monkey.get_id(), parcel);
+        monkeys.push(monkey);
+        if let None = lines.next() {
+            break;
+        }
+    }
+
+    Ok((parcels, monkeys))
 }
--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,12 +1,32 @@
-use std::{fs::File, io::{BufReader, BufRead}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn main() -> std::io::Result<()> {
-    let f: File = File::open("src/input.txt")?;
+    let f: File = File::open("src/input.txt~")?;
     let outcome = process_lines(BufReader::new(f).lines())?;
+    println!("{}", outcome);
     Ok(())
 }
 
-fn process_lines(lines: std::io::Lines<BufReader<File>>) -> std::io::Result<Vec<String>> {
-
+fn process_lines(mut lines: std::io::Lines<BufReader<File>>) -> std::io::Result<usize> {
+    let Ok((mut parcels, monkeys))
+        = day11::parse_monkeys(&mut lines) else { panic!("process_lines") };
+    let mut levels: Vec<usize> = monkeys.iter().map(|_| 0usize).collect();
+    let modulus : u64 = monkeys.iter().map(day11::MonkeyBrain::get_modulus).product();
+    (0..10_000).for_each(|_| {
+        monkeys.iter().for_each(|monkey| {
+            let id = monkey.get_id();
+            if let Some(mut mine) = parcels.remove(&id) {
+                mine.drain(..).for_each(|item| {
+                    let (ix, value) = monkey.inspect_mod(item, modulus);
+                    levels[id] += 1;
+                    parcels.entry(ix).or_default().push(value);
+                })
+            }
+        })
+    });
+    levels.sort_by(|a, b| b.cmp(a));
+    Ok(levels[0] * levels[1])
 }
-
